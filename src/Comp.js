5872cd4394ed5af5aaab2751c9bfb7377ec374bf
Namman Goyal <nammangoyal@gmail.com>
2024-08-16 01:34:58 +0530

    changes

--- src/Comp.js
+++ src/Comp.js
@@ -1,43 +1,51 @@
 import React from 'react';
 import ResponsiveAppBar from './ResponsiveAppBar';
 import './tailwind.css';
-
-import { Button, Typography, Container } from '@mui/material';
+import { Typography } from '@mui/material';
 import Movies from './Movies';
 import Searchs from './Searchs';
 import Footer from './Footer';
 
 function Comp() {
   return (
-    <div className='h-screen '>
+    <div className="h-screen">
       
 
-      <div className='w-full max-w-7xl mx-auto h-1/2 bg-slate-600 pl-[5%] md:top-[64px] sm:top-[55.99px] opacity-65 absolute left-1/2  transform -translate-x-1/2'>
-        
-        
-      </div>
-      <div className="w-full max-w-7xl mx-auto h-1/2 bg-[url('../public/5.jpg')] pt-20 pl-[5%] bg-cover">
-        <Searchs/>
       
+      <div className="relative w-full max-w-7xl mx-auto h-1/2">
+        
+        <div className="absolute inset-0 bg-cover bg-[url('../public/5.jpg')] bg-center z-0"></div>
+        
+        
+        <div className="absolute inset-0 bg-slate-600 opacity-70 z-10"></div>
+
+        
+        <div className="relative z-20 pt-20 pl-[5%]">
+          <Searchs />
         </div>
-      
-      <div className='bg-customColor'>
-      <Typography variant="h5" gutterBottom className='pt-10 pl-5'>
-        Popular
-      </Typography>
-      
-      <Movies category='popular'/>
-      <br></br>
       </div>
-      <div className='bg-customColorr pb-10'>
-      <Typography variant="h5" gutterBottom className='pl-5 pt-10'>
-        Trending
-      </Typography>
-      <Movies category='trending'/>
+
+      
+      <div className="bg-customColor">
+        <Typography variant="h5" gutterBottom className="pt-10 pl-5">
+          Popular
+        </Typography>
+        <Movies category="popular" />
+        <br />
       </div>
-      <Footer/>
+
+      
+      <div className="bg-customColorr pb-10">
+        <Typography variant="h5" gutterBottom className="pl-5 pt-10">
+          Trending
+        </Typography>
+        <Movies category="trending" />
+      </div>
+
+      
+      <Footer />
     </div>
   );
 }
 
-export default Comp; 
+export default Comp;
